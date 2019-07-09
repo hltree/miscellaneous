f9b24cfd5f1ca2879c9cb41e468e876583f2dd05
kiona <kiona@kionanoMacBook-Pro.local>
2019-07-10 01:03:28 +0900

    finish 4.3.3

--- php/slim/tutorial/Tutorial-First-Application/src/dependencies.php
+++ php/slim/tutorial/Tutorial-First-Application/src/dependencies.php
@@ -19,4 +19,14 @@ return function (App $app) {
         $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
         return $logger;
     };
+
+    // database
+    $container['db'] = function($c) {
+        $db = $c['setting']['db'];
+        $pdo = new PDO('mysql:host=' . $db['host'] . '; dbname=' . $db['dbname'],
+            $db['user'], $db['pass']);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    };
 };
