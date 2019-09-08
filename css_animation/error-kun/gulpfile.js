597ac69441ee89e34ca78a26a6471cbab146d662
hltree <kiona.n31@icloud.com>
2019-09-08 11:17:40 +0900

    update: change process

--- css_animation/error-kun/gulpfile.js
+++ css_animation/error-kun/gulpfile.js
@@ -1,32 +1,39 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
+const
+    browserSync = require('browser-sync'),
+    gulp = require('gulp'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
     pug = require('gulp-pug'),
-    connect = require('gulp-connect');
+    sass = require('gulp-sass');
 
-gulp.task('compile-scss', function () {
+gulp.task('compile-scss', () => {
     return (
         gulp
             .src('./dev/scss/style.scss')
+            .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
             .pipe(sass())
             .pipe(gulp.dest('./build/css'))
     );
 });
-gulp.watch('./dev/scss/*', gulp.task('compile-scss'));
 
-gulp.task('compile-pug', function () {
+gulp.task('compile-pug', () => {
     return (
         gulp
             .src('./dev/pug/*.pug')
+            .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
             .pipe(pug())
             .pipe(gulp.dest('./build/view'))
     );
 });
-gulp.watch('./dev/pug/*', gulp.task('compile-pug'));
 
-gulp.task('connect', function(){
-    connect.server({
-        root: './build/view'
+gulp.task('browser-sync', () => {
+    browserSync({
+        server: {
+            baseDir: './build/view'
+        }
     });
+    gulp.watch('./dev/scss/*', gulp.task('compile-scss'));
+    gulp.watch('./dev/pug/*', gulp.task('compile-pug'));
 });
 
-gulp.task('default', gulp.series(gulp.parallel('compile-pug', 'compile-scss', 'connect')));
+gulp.task('default', gulp.series(gulp.parallel('browser-sync', 'compile-pug', 'compile-scss')));
