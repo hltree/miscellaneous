58be951b2a79cc3771f89a8bb4e847b81adebad6
hltree <kiona.n31@icloud.com>
2019-09-09 17:39:28 +0900

    update: add move random error-kun

--- css_animation/error-kun/dev/js/common.js
+++ css_animation/error-kun/dev/js/common.js
@@ -1,2 +1,18 @@
 // moduleの読み込みはindexのほうで
 
+const error = $('.error-kun'),
+    inner = $('.inner'),
+    half_margin_x = (inner.width() - error.width()) / 2,
+    half_margin_y = (inner.height() - error.height()) / 2;
+
+TweenMax.staggerTo(error, .1, {rotation:360, repeat: -1});
+
+const move_error = () => {
+    let x = Math.floor(((Math.random() * 2) - 1) * half_margin_x),
+        y = Math.floor(((Math.random() * 2) - 1) * half_margin_y);
+    TweenMax.to(error, 1, {x:x, y:y, onComplete: () => {
+        move_error();
+    }});
+};
+
+move_error();
