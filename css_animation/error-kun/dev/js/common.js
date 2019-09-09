018925ceaf77e72670504068c666ce46ea54da46
hltree <kiona.n31@icloud.com>
2019-09-10 01:52:04 +0900

    update: add template

--- css_animation/error-kun/dev/js/common.js
+++ css_animation/error-kun/dev/js/common.js
@@ -5,14 +5,16 @@ const error = $('.error-kun'),
     half_margin_x = (inner.width() - error.width()) / 2,
     half_margin_y = (inner.height() - error.height()) / 2;
 
-TweenMax.staggerTo(error, .1, {rotation:360, repeat: -1});
+if($('body').hasClass('mode-rotate')) {
+    TweenMax.staggerTo(error, .1, {rotation:360, repeat: -1});
 
-const move_error = () => {
-    let x = Math.floor(((Math.random() * 2) - 1) * half_margin_x),
-        y = Math.floor(((Math.random() * 2) - 1) * half_margin_y);
-    TweenMax.to(error, 1, {x:x, y:y, onComplete: () => {
-        move_error();
-    }});
-};
+    const move_error = () => {
+        let x = Math.floor(((Math.random() * 2) - 1) * half_margin_x),
+            y = Math.floor(((Math.random() * 2) - 1) * half_margin_y);
+        TweenMax.to(error, 1, {x:x, y:y, onComplete: () => {
+                move_error();
+            }});
+    };
 
-move_error();
+    move_error();
+}
