f132b415429b06fa140ab7c3c71afc028268107d
hltree <kiona.n31@icloud.com>
2019-09-19 22:30:35 +0900

    update: fixed content

--- js/browser_back/dev/js/main.js
+++ js/browser_back/dev/js/main.js
@@ -1,6 +1,22 @@
-const retentionWP_cookie = document.cookie.indexOf('cookie_rwt');
-if(retentionWP_cookie) {
-    $(window).scrollTop(retentionWP_cookie);
+const cookies = document.cookie,
+    cookies_array = cookies.split(';');
+if(cookies_array) {
+    let split = new Array(),
+        judge = false,
+        result = new Array();
+    for(let i=0; i<cookies_array.length; i++) {
+        split = cookies_array[i].split('=');
+        split[0] = split[0].replace(/\s+/g, '');
+        split[1] = split[1].replace(/\s+/g, '');
+        result[split[0]] = split[1];
+
+        if(split[0] == 'cookie_rwt') {
+            judge = true;
+        }
+    }
+    if(judge == true) {
+        $(window).scrollTop(result['cookie_rwt']);
+    }
 }
 
 $(window).on('beforeunload', function(){
