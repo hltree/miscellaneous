0c378aafaba8d419a6ce15180ecd624d5df97420
hltree <kiona.n31@icloud.com>
2019-09-19 23:20:45 +0900

    update: env set

--- js/infinite_scroll/gulpfile.js
+++ js/infinite_scroll/gulpfile.js
@@ -2,39 +2,45 @@ const
     bsync = require('browser-sync'),
     gulp = require('gulp'),
     pug = require('gulp-pug'),
-    sass = require('gulp-sass'),
-    notify = require('gulp-notify');
+    sass = require('gulp-sass');
 
 gulp.task('pug', () => {
     return (
         gulp.src('./dev/pug/*.pug')
-        pipe.(pug())
-        pipe.(dest('./'))
+            .pipe(pug())
+            .pipe(gulp.dest('./'))
     );
 });
 
 gulp.task('sass', () => {
     return (
         gulp.src('./dev/scss/*.scss')
-        pipe.(sass())
-        pipe.(dest('./build/css'))
+            .pipe(sass())
+            .pipe(gulp.dest('./build/css'))
     );
 });
 
 gulp.task('js', () => {
-    return(
+    return (
         gulp.src('./dev/js/*.js')
-        pipe.(dest('./build/js'))
+            .pipe(gulp.dest('./build/js'))
     );
 });
 
-gulp.task('reload', (done)=>{
+gulp.task('reload', (done) => {
     bsync.reload();
-    dorn();
+    done();
 });
 
 gulp.task('bsync', () => {
-
+    bsync({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./dev/pug/*.pug', gulp.series('pug', 'reload'));
+    gulp.watch('./dev/scss/*.scss', gulp.series('sass', 'reload'));
+    gulp.watch('./dev/js/*.js', gulp.series('js', 'reload'));
 });
 
-gulp.task('default', '');
+gulp.task('default', gulp.series('pug', 'sass', 'js', 'bsync'));
