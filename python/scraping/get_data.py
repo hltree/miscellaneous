6f30f5d354e43700e6977b55d36fec3c39254eea
hltree <kiona.n31@icloud.com>
2019-10-23 11:22:52 +0900

    update: add Process

--- python/scraping/get_data.py
+++ python/scraping/get_data.py
@@ -7,4 +7,21 @@ url = config.init()
 html = urllib.request.urlopen(url)
 soup = BeautifulSoup(html, 'html.parser')
 
-print(html)
+div = soup.find_all('div')
+
+array = []
+
+for tag in div:
+    try:
+        id = tag.get('id')
+        if id == 'kenkyu_box':
+            img = tag.find('img')
+            array.append(img['src'])
+    except:
+        pass
+
+for val in array:
+    val = val + '\n'
+    file = open('./result.txt', 'a')
+    file.write(val)
+    file.close
