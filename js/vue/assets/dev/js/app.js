33a6851757ad6171ac90ac57951c6740a73fa6df
hltree <kiona.n31@icloud.com>
2019-12-11 23:55:04 +0900

    update: DeleteApp

--- js/vue/assets/dev/js/app.js
+++
@@ -1,9 +0,0 @@
-const
-    $ = require('jquery');
-    Vue = require('vue');
-
-Vue.component('todo-item', {
-    template: '<li>This is a todo</li>'
-});
-
-let app = new Vue();
--- js/vue/assets/src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import app from './app'
-
-new Vue({
-    el: '#app',
-    template: '<App />',
-    components: { App }
-});
