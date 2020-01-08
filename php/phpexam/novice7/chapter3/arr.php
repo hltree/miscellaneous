d0e44986c69fd1bf11312354252ef9ec75ae64ad
hltree <kiona.n31@icloud.com>
2020-01-08 23:52:05 +0900

    update: chapter3

---
+++ php/phpexam/novice7/chapter3/arr.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * fatal error!
+$arr = 'test';
+$arr[] = 'array';
+echo $arr;
+*/
+
+/*
+ * Warning!
+$arr = 10;
+$arr[] = 'aa';
+echo $arr;
+*/
+
+/*
+ * Warning!
+ * 加えて、配列の値がおかしくなる！
+$arr = 'text';
+$arr['key'] = 'value';
+
+echo $arr;
+*/
