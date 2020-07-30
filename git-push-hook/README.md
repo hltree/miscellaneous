8750a70b359a63fe92633c3bd1bb31bca244faa1
hltree <infon@hltree.tech>
2020-07-30 12:26:21 +0900

    update: test git-hook

---
+++ git-push-hook/README.md
@@ -0,0 +1,25 @@
+# git-push-hook
+
+## Overview
+
+シェルスクリプトで同階層の `.git` の中にhookを生成します。
+
+hookは `git push` 時に `.git-push-hook-config` に設定された環境にコマンドを入力させます。
+
+## Usage
+
+### 1. .git-push-hook-config(設定ファイル)をコピーしてください
+
+```
+$ cp .git-push-hook-config.sample .git-push-hook-config
+```
+
+### 2. 1で作成したファイルとshファイルを移動させてください
+
+1で作成した `.git-push-hook-config` と `generate-hook.sh` を適用させたいgitリポジトリのディレクトリに移動させてください。
+
+```
+$ mv .git-push-hook-config generate-hook.sh ${target}
+```
+
+### 3. 
---
+++ git-push-hook/generate-hook.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GIT_DIR=$(dirname $0)/.test
+
+if [[ -d $GIT_DIR ]]; then
+    echo "Directory exists!"
+    mkdir $GIT_DIR/hooks
+    cat <<EOF > $GIT_DIR/hooks/pre-push
+#!/bin/bash
+
+echo '[warn] push to remote, continue? [y/N]'
+
+exec < /dev/tty
+read answer
+
+case $answer in
+    'y' | 'yes') echo '[info] OK. push start.';;
+    * ) echo '[error] push failed.';exit 1;;
+esac
+exit 0
+EOF
+    chmod +x $GIT_DIR/hooks/pre-push
+    exit 0
+else
+  echo "Directory not exists..."
+    exit 1
+fi
