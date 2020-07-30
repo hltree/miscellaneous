cd84440360582770e59f4bb451badd40602d4ff4
hltree <infon@hltree.tech>
2020-07-30 15:52:59 +0900

    chore: update git-hook

--- generate-hook.sh
+++ generate-hook.sh
@@ -6,7 +6,7 @@ if [[ -d $GIT_DIR ]]; then
     echo "Directory exists!"
     mkdir $GIT_DIR/hooks
     echo $GIT_DIR/hooks/deploy.sh
-    git config --local alias.push '!git push $1 $2'" && sh $GIT_DIR/hooks/deploy.sh"
+    git config --local alias.dpush '!git push $1 $2 && if $2 = '"$REPOSITORY_BASE_BRANCH; then sh $GIT_DIR/hooks/deploy.sh fi"
 
     cat <<'EOS' > $GIT_DIR/hooks/deploy.sh
 #!bin/bash
@@ -20,9 +20,19 @@ read ANSWER
 case $ANSWER in "Y" | "y" | "yes" | "Yes" | "YES" ) echo "OK. pull start.";;
 * ) echo "pull failed.";exit 1;;
 esac
+
 ssh -i $SSH_IDENTITY_FILE_PATH $SERVER_USER_NAME@$SERVER_HOST_NAME <<EOC
 cd $GIT_PULL_DIR_PATH
-git clone $REPOSITORY_SSH_URL
+
+if [[ -d $GIT_PULL_DIR_PATH ]]; then
+  if [[ -d $GIT_PULL_DIR_PATH/.git ]]; then
+    git clone $REPOSITORY_SSH_URL .
+  else
+    git pull origin $REPOSITORY_BASE_BRANCH
+  fi
+else
+  echo "Server directory not exists..."
+fi
 EOC
 
 exit 0
