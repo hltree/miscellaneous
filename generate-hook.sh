c85e68b2658d385245b425dfaa04fe4431d6999a
hltree <infon@hltree.tech>
2020-07-30 14:43:00 +0900

    chore: update git-hook

--- generate-hook.sh
+++ generate-hook.sh
@@ -5,7 +5,19 @@ GIT_DIR=$(dirname $0)/.git
 if [[ -d $GIT_DIR ]]; then
     echo "Directory exists!"
     mkdir $GIT_DIR/hooks
-    cat <<'EOS' > $GIT_DIR/hooks/post-receive
+    git config --local alias.push 'push && $GIT_DIR/hooks/deploy.sh'
+    cat <<'EOS' > $GIT_DIR/hooks/deploy.sh
+#!bin/bash
+# 設定ファイルから#以外をよみこむ
+export $(cat $(dirname $0)/.git-push-hook-config | grep -v ^# | xargs);
+
+ssh -i $SSH_IDENTITY_FILE_PATH $SERVER_USER_NAME@$SERVER_HOST_NAME;
+cd $GIT_PULL_DIR_PATH;
+git clone $REPOSITORY_SSH_URL;
+EOS
+
+
+    cat <<'EOS' > $GIT_DIR/hooks/pre-push
 #!/bin/bash
 
 # 設定ファイルから#以外をよみこむ
@@ -13,7 +25,7 @@ export $(cat $(dirname $0)/.git-push-hook-config | grep -v ^# | xargs);
 
 while read local_ref local_sha1 remote_ref remote_sha1
 do
-  #if [[ "${remote_ref##refs/heads/}" = $REPOSITORY_BASE_BRANCH ]]; then
+  if [[ "${remote_ref##refs/heads/}" = $REPOSITORY_BASE_BRANCH ]]; then
     echo "Warning: pull to remote server, continue? [y/N]"
 
     exec < /dev/tty
@@ -22,15 +34,11 @@ do
     case $ANSWER in "Y" | "y" | "yes" | "Yes" | "YES" ) echo "OK. pull start.";;
     * ) echo "push failed.";exit 1;;
     esac
-
-    ssh -i $SSH_IDENTITY_FILE_PATH $SERVER_USER_NAME@$SERVER_HOST_NAME;
-    cd $GIT_PULL_DIR_PATH;
-    git clone $REPOSITORY_SSH_URL;
     exit 0
-  #fi
+  fi
 done
 EOS
-    chmod +x $GIT_DIR/hooks/post-receive
+    chmod +x $GIT_DIR/hooks/pre-push
     exit 0
 else
   echo "Directory not exists..."
