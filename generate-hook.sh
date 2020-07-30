67c4429a0dd50b517c777b3b49c4251c310be956
hltree <infon@hltree.tech>
2020-07-30 16:17:06 +0900

    chore: update git-hook

--- generate-hook.sh
+++ generate-hook.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # variables
 GIT_DIR=$(dirname $0)/.git
-export $(cat $GIT_DIR/hooks/.GIT_PUSH_HOOK_CONFIG | grep REPOSITORY_BASE_BRANCH | xargs);
-
 
 if [[ -d $GIT_DIR ]]; then
     echo "Directory exists!"
     mkdir $GIT_DIR/hooks
     echo $GIT_DIR/hooks/deploy.sh
-    git config --local alias.dpush '!git push $1 $2 && if $2 = '"$REPOSITORY_BASE_BRANCH;then sh $GIT_DIR/hooks/deploy.sh;fi"
+    git config --local alias.dpush '!git push $1 $2 && '"sh $GIT_DIR/hooks/deploy.sh;fi"
 
     cat <<'EOS' > $GIT_DIR/hooks/deploy.sh
 #!bin/bash
