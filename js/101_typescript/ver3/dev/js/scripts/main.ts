d107c10d7309d975fef77d27c648bed8d96ab9a6
hltree <35206336+hltree@users.noreply.github.com>
2022-02-08 11:27:12 +0900

    update: typescriptでwpの投稿を取得して表示する

--- js/101_typescript/ver3/dev/js/scripts/main.ts
+++ js/101_typescript/ver3/dev/js/scripts/main.ts
@@ -4,6 +4,7 @@ import SpNavi from "./modules/SpNavi";
 import ToggleToTop from "./modules/ToggleToTop";
 import SetLinkClasses from "./modules/SetLinkClasses";
 import MatchMedia from "./modules/MatchMedia";
+
 /**
  * ブラウザ判定用のクラスを設定する
  */
@@ -25,7 +26,7 @@ new SetLinkClasses();
 //   lg : 1024,
 //   xl : 1280
 // }
-const mm  = new MatchMedia();
+const mm = new MatchMedia();
 // console.log(mm.is('sm'));
 // console.log(mm.is('md'));
 // console.log(mm.is('lg'));
@@ -45,6 +46,87 @@ new SpNavi();
  */
 new SmoothScroll();
 
+/**
+ * 投稿を取得してひょうじするやつ
+ */
+const postWrapper = document.querySelector('.posts')
+if (null !== postWrapper) {
+	fetch('https://s1.demo.opensourcecms.com/wordpress/wp-json/wp/v2/posts')
+		.then((response) => {
+			return response.json()
+		})
+		.then((jsonData) => {
+			jsonData.forEach((post) => {
+				const title = document.createElement('summary')
+				title.textContent = post.title.rendered
+				title.classList.add('title')
+
+				const content = document.createElement('div')
+				content.textContent = striptags(post.content.rendered).slice(0, 100) + '...'
+				content.classList.add('content')
+
+				if (title && content) {
+					const details = document.createElement('details')
+					details.classList.add('post')
+					details.append(title, content)
+					title.addEventListener('click', () => {
+						title.classList.add('show')
+					})
+					postWrapper.append(details)
+				}
+			})
+		})
+}
+
+
+import striptags from "striptags";
+import validUrl from 'valid-url'
+
+interface Params {
+	url: string,
+	wrapperElementName: string
+}
+
+class PostListError extends Error {}
+
+class PostList {
+	private callBack: Function | null = null
+	private url: string | null = null
+	private wrapperElement: HTMLElement | null
+
+	constructor(params: Params, callBack: null | Function = null) {
+		this.url = params.url
+		this.wrapperElement = document.querySelector<HTMLElement>(params.wrapperElementName)
+		this.callBack = <Function>callBack
+
+		this.checkUrl()
+	}
+
+	checkUrl() {
+		if (!validUrl.isUri(this.url)) {
+			throw new PostListError()
+		}
+	}
+
+	getCallBack(): Function | null {
+		return this.callBack
+	}
+
+	getUrl(): string | null {
+		return this.url
+	}
+
+	getWrapperElement(): HTMLElement | null {
+		return this.wrapperElement
+	}
+}
+
+const ClassPostList = new PostList({
+	url: 'aaa',
+	wrapperElementName: 'eee'
+})
+
+
 if (module.hot) {
-  module.hot.accept();
+	module.hot.accept();
 }
